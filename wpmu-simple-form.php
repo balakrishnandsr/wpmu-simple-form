2414e0f818a186f6f085520950b649d66c87e301
Balakrishnan <baladsr72@gmail.com>
2023-11-27 11:26:49 +0530

    Created Plugin Skeleton

---
+++ wpmu-simple-form.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     WPMU Demo Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpmu-simple-form
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpmu_Simple_Form
+ */
+
+// Your code starts here.
