0eddbe9fa16430310b58004d30c72c3f5581defb
Balakrishnan <baladsr72@gmail.com>
2023-11-28 22:31:32 +0530

    Generated Pot file and added Pagination page next and previous

--- includes/assets/js/wpmusf-script.js
+++ includes/assets/js/wpmusf-script.js
@@ -61,6 +61,7 @@
 			search_pagination({ method: 'search_wpmu_simple_form', action: 'wpmu_ajax', wpmu_search_key: key, search_nonce: nonce });
 
 		} );
+
 		/**
 		 * search Pagination prev
 		 */
@@ -68,11 +69,11 @@
 			let offset = $(this).data("offset");
 			let nonce = $(this).data("nonce");
 			let key = $(this).attr("data-key");
-			search_pagination({ method: 'search_wpmu_simple_form', action: 'wpmu_ajax', wpmu_search_key: key, search_nonce: nonce });
+			search_pagination({ method: 'search_wpmu_simple_form', action: 'wpmu_ajax', wpmu_search_key: key, search_nonce: nonce, offset:offset });
 		} );
 
 		/**
-		 * Common Function for pagination
+		 * Common Function for search pagination
 		 * @param data
 		 */
 		function search_pagination( data ){
@@ -85,9 +86,39 @@
 					alert('OOPs!! Something Went Wrong, Please try again later.');
 				},
 				success: function (response) {
-					//$('#wpmu_search').trigger("reset");
-					$('#wpmu_search_button').prop('disabled', false);
-					$('.wpmu_search_message').html(response.data.message);
+					$('.mpmu-form-list').html(response.data.list);
+				}
+			});
+		}
+
+		$('body').on( 'click', '.wpmu_list_prev', function (){
+			let offset = $(this).data("offset");
+			let nonce = $(this).data("nonce");
+
+			list_pagination({ method: 'wpmu_simple_form_list', action: 'wpmu_ajax', search_nonce: nonce, offset: offset });
+		});
+
+		$('body').on( 'click', '.wpmu_list_next', function (){
+			let offset = $(this).data("offset");
+			let nonce = $(this).data("nonce");
+
+			list_pagination({ method: 'wpmu_simple_form_list', action: 'wpmu_ajax', search_nonce: nonce, offset: offset });
+		});
+
+		/**
+		 * Common Function for list pagination
+		 * @param data
+		 */
+		function list_pagination( data ){
+			$.ajax({
+				data: data,
+				type: 'post',
+				url: custom_script.ajaxurl,
+
+				error: function (request, error) {
+					alert('OOPs!! Something Went Wrong, Please try again later.');
+				},
+				success: function (response) {
 					$('.mpmu-form-list').html(response.data.list);
 				}
 			});
